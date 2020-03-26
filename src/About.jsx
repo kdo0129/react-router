f7ad2459b58949bcf0d470e2fb16225f36d72846
kdo0129 <eha0129@naver.com>
2020-03-26 19:45:42 +0900

    add Home, About Route_ Link

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<>
+			<h1>About 페이지</h1>
+			<p>About 페이지 내용입니다.</p>
+		</>
+	);
+};
+
+export default About;
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<>
+			<h1>홈 화면</h1>
+			<p>라우터 튜토리얼 홈 화면입니다.</p>
+		</>
+	);
+};
+
+export default Home;
