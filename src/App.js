6a9eabbd66ebfa9fbb3607f044982dbeca7fed71
kdo0129 <eha0129@naver.com>
2020-03-26 22:32:38 +0900

    withRouter 적용, Switch 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
@@ -22,10 +22,20 @@ function App() {
 					<Link to="/history">History 예제</Link>
 				</li>
 			</ul>
-			<Route path="/" component={Home} exact />
-			<Route path="/about" component={About} />
-			<Route path="/profiles" component={Profiles} />
-			<Route path="/history" component={HistorySample} />
+			<Switch>
+				<Route path="/" component={Home} exact />
+				<Route path="/about" component={About} />
+				<Route path="/profiles" component={Profiles} />
+				<Route path="/history" component={HistorySample} />
+				<Route
+					render={({ location }) => (
+						<div>
+							<h2>이페이지는 존재하지 않습니다.</h2>
+							<p>{location.pathname}</p>
+						</div>
+					)}
+				/>
+			</Switch>
 		</div>
 	);
 }
